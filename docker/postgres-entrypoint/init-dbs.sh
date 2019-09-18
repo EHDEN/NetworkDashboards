40ca3c83818ddc95041176932b503863e15842d7
aspedrosa <aspedrosa@ua.pt>
2019-09-19 00:05:10 +0100

    Add compose files
    Next commits should add
    - a README to this folder
    - change the dashboard viewer app so it uses the environment variables
    received from the docker-compose

---
+++ docker/postgres-entrypoint/init-dbs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# based of https://github.com/mrts/docker-postgresql-multiple-databases
+
+set -eu
+
+if [ -n "$POSTGRES_DBS" ] ; then
+  # Get arrays with information for the several databases to create
+  # Received through docker-compose environment variables
+  declare -a DBS=($POSTGRES_DBS)
+  declare -a USERS=($POSTGRES_DBS_USERS)
+  declare -a PASSWORDS=($POSTGRES_DBS_PASSWORDS)
+
+  # Check if all have the same lenght
+  if ! [ ${#USERS[@]} -eq ${#DBS[@]} ] || ! [ ${#DBS[@]} -eq ${#PASSWORDS[@]} ] ; then
+    echo "Different size of the variables POSTGRES_DBS, POSTGRES_DBS_USERS and POSTGRES_DBS_PASSWORDS"
+    exit 1
+  fi
+
+  for i in $(seq 0 $((${#DBS[@]}-1)) ) ; do
+    DB=${DBS[$i]}
+    USER=${USERS[$i]}
+    PASSWORD=${PASSWORDS[$i]}
+
+    psql -c "CREATE USER $USER WITH PASSWORD '$PASSWORD'" -U root
+    psql -c "CREATE DATABASE $DB" -U root
+    psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB TO $USER" -U root
+  done
+fi
