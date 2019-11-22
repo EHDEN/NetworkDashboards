dc3d25e0c16397227c27d2286dd14c017dc4f89e
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2019-11-22 00:00:13 +0000

    Upgrade the feature for upload CSV Files (Presented in 14-Nov TC)

--- dashboard_viewer/uploader/models.py
+++ dashboard_viewer/uploader/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class AchillesResults(models.Model):
+	class Meta:
+		db_table = "AchillesResults"
+
+	source		= models.TextField()
+	analysis_id	= models.BigIntegerField()
+	stratum_1	= models.TextField()
+	stratum_2	= models.TextField()
+	stratum_3	= models.TextField()
+	stratum_4	= models.TextField()
+	stratum_5	= models.TextField()
+	count_value	= models.BigIntegerField()
+
+class Uploads(models.Model):
+	pass
--- dashboard_viewer/uploader/views.py
+++ dashboard_viewer/uploader/views.py
@@ -4,46 +4,54 @@ import pandas as pd
 import sqlalchemy as sa
 from django.conf import settings
 from sqlalchemy import create_engine
+from .models import AchillesResults
+
 
 
 def upload(request, *args, **kwargs):
-    if request.method == 'POST':
-        if "db" not in kwargs:
-            pass #raise issue
+    if "db" not in kwargs:
+        db = None
+    else:
         db = kwargs["db"]
-        uploadedFile = request.FILES["achillesResults"]
-        if uploadedFile.content_type == "text/csv":
-            READ = False
-            fileContent = []
-            for line in uploadedFile:
-                if READ:
-                    newLine = line.decode('ASCII').strip().replace('"', "")
-                    newLine = [db] + newLine.split(",")
-                    fileContent += [newLine]
-                else:
-                    READ = True
-            df = pd.DataFrame(fileContent, columns=("source","analysis_id","stratum_1","stratum_2","stratum_3","stratum_4","stratum_5","count_value"))
-            insertIntoDB(df)
-    return render(request, 'upload.html')
+    success = False
 
-def insertIntoDB(df):
-    engine = create_engine("postgresql://"+
-                           settings.DATABASES['default']['USER']+":"+settings.DATABASES['default']['PASSWORD']+"@"+
-                           settings.DATABASES['default']['HOST']+":"+settings.DATABASES['default']['PORT']+"/"+
-                           settings.DATABASES['default']['NAME'])
+    if request.method == 'POST':
+        if db == None and 'inputDB' in request.POST:
+            db = request.POST['inputDB']
 
-    dtype = {
-        "source":       sa.types.String,
-        "analysis_id":  sa.types.BigInteger,
-        "stratum_1":    sa.types.String,
-        "stratum_2":    sa.types.String,
-        "stratum_3":    sa.types.String,
-        "stratum_4":    sa.types.String,
-        "stratum_5":    sa.types.String,
-        "count_value":  sa.types.BigInteger
+        if db != None:
+            uploadedFile = request.FILES["achillesResults"]
+            if uploadedFile.content_type == "text/csv":
+                READ = False
+                listOfEntries = []
+                for line in uploadedFile:
+                    if READ:
+                        listOfEntries.append(buildEntry(db, line))
+                    else:
+                        READ = True
+
+                insertIntoDB(listOfEntries, db)
+        success = True
+
+    context = {
+        "db": db,
+        "success": success
     }
-    df.to_sql("achilles_results", engine,   if_exists  = 'append',
-                                            index      = False,
-                                            schema     = "public",
-                                            dtype      = dtype)
-   
+    return render(request, 'upload.html', context)
+
+def buildEntry(db, line):
+    #columns=("source","analysis_id","stratum_1","stratum_2","stratum_3","stratum_4","stratum_5","count_value")    
+    newLine = line.decode('ASCII').strip().replace('"', "")
+    newLine = [db] + newLine.split(",")
+    return AchillesResults(source         = newLine[0],
+                           analysis_id    = newLine[1],
+                           stratum_1      = newLine[2],
+                           stratum_2      = newLine[3],
+                           stratum_3      = newLine[4],
+                           stratum_4      = newLine[5],
+                           stratum_5      = newLine[6],
+                           count_value    = newLine[7])
+
+
+def insertIntoDB(listOfEntries, database):
+    AchillesResults.objects.bulk_create(listOfEntries)
