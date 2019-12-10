66d3406726bc86bff02c4c1aed7da972184534a8
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2019-12-10 17:03:50 +0000

    Add gunicorn for the production deployment

--- dashboard_viewer/docker-entrypoint.sh
+++ dashboard_viewer/docker-entrypoint.sh
@@ -3,5 +3,9 @@
 set -ex
 
 # TODO create two cases: production and development
-
-python manage.py runserver 0.0.0.0:8000
+if [ ${DASHBOARD_VIEWER_ENV} = "production" ]; then
+	python manage.py collectstatic --noinput
+	exec gunicorn genericcdss.wsgi:application --bind 0.0.0.0:8000 --workers 5
+else
+	python manage.py runserver 0.0.0.0:8000
+fi
