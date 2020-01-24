d8203ae33f1937a2d9d4cbde355f79a75b861eac
aspedrosa <aspedrosa@ua.pt>
2020-01-24 12:57:36 +0000

    Changes on tabs models to support submenus

--- dashboard_viewer/tabsManager/admin.py
+++ dashboard_viewer/tabsManager/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 
-from .models import Tab
+from .models import Tab, TabGroup
 
 admin.site.register(Tab)
+admin.site.register(TabGroup)
--- dashboard_viewer/tabsManager/models.py
+++ dashboard_viewer/tabsManager/models.py
@@ -1,8 +1,15 @@
 
 from django.db import models
+from model_utils.managers import InheritanceManager
 
 
-class Tab(models.Model):
+class Button(models.Model):
+    """
+    Base class for button on the left bar
+    """
+
+    objects = InheritanceManager()
+
     title        = models.CharField(
         max_length=30,
         help_text="Text to appear on the tab under the icon"
@@ -11,7 +18,6 @@ class Tab(models.Model):
         max_length=20,
         help_text="Font awesome icon v4. Just the end part, e.g. fa-clock-o -> clock-o"
     )
-    url          = models.URLField()
     position     = models.PositiveIntegerField()
     visible      = models.BooleanField(
         help_text="If the tab should be displayed"
@@ -22,3 +28,20 @@ class Tab(models.Model):
 
     def __str__(self):
         return f"{self.title}, position: {self.position}, visible: {self.visible}"
+
+
+class TabGroup(Button):
+    """
+    Type of buttons that can hold a submenu
+    Dont't display iframes
+    """
+    pass
+
+
+class Tab(Button):
+    """
+    Type of buttons that display a iframe
+    Can be within a group, forming a submenu
+    """
+    url          = models.URLField(null=True)
+    group = models.ForeignKey(TabGroup, on_delete=models.SET_NULL, null=True, blank=True)
