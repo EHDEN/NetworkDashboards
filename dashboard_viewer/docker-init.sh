bba2824ecbd4f31e5fa96bc2c6c613f20d125f7d
aspedrosa <aspedrosa@ua.pt>
2020-02-01 11:06:41 +0000

    Update docker initialization script
    - support new achilles database

--- dashboard_viewer/docker-init.sh
+++ dashboard_viewer/docker-init.sh
@@ -4,6 +4,7 @@ set -ex
 
 # Apply django migrations
 python manage.py migrate
+python manage.py migrate --database=achilles uploader
 
 # Create an user for the admin app
 python manage.py createsuperuser
