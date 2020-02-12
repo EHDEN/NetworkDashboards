fa4913cfdc1d1a4624fa00b8e2b48b32a1b7b884
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2020-02-12 15:40:44 +0000

    Configs to simplify the installation

--- dashboard_viewer/docker-entrypoint.sh
+++ dashboard_viewer/docker-entrypoint.sh
@@ -4,4 +4,8 @@ set -ex
 
 # TODO create two cases: production and development
 
-python manage.py runserver 0.0.0.0:8000
+if [ ${DASHBOARD_VIEWER_ENV} = "production" ]; then
+	exec gunicorn dashboard_viewer.wsgi:application --bind 0.0.0.0:8000 --workers 5
+else
+	python manage.py runserver 0.0.0.0:8000
+fi 
