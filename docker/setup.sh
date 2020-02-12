75d4accc593b216fca318cb80d5ef894f99f8e75
aspedrosa <aspedrosa@ua.pt>
2020-02-12 16:37:57 +0000

    Set up cache for superset's charts

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,10 +3,7 @@
 echo "Setting up superset repo"
 git clone https://github.com/apache/incubator-superset ../superset
 
-cp ../superset/contrib/docker/superset_config.py ../superset
-
 echo "Setting up the database for the superset app"
-docker-compose run --rm -e FLASK_APP=superset superset flask fab create-db
 docker-compose run --rm superset ./docker-init.sh
 
 echo "Setting up the database for the dashboard viewer app"
