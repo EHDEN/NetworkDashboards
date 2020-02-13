64feec251ba1b98a57f57076ad9de64c71fd72e8
aspedrosa <aspedrosa@ua.pt>
2020-02-13 18:12:53 +0000

    Revert tabs manager changes

--- dashboard_viewer/tabsManager/views.py
+++ dashboard_viewer/tabsManager/views.py
@@ -2,7 +2,7 @@
 from django import views
 from django.shortcuts import render
 
-from .models import Tab, TabGroup, Button
+from .models import Tab
 
 
 class TabsView(views.View):
@@ -10,49 +10,16 @@ class TabsView(views.View):
 
     def get(self, request, *args, **kwargs):
 
-        # get all base visible buttons, ordered by their position and title fields
-        buttons = Button.objects.filter(visible=True).order_by("position", "title").select_subclasses()
+        tabs = Tab.objects.filter(visible=True).order_by("position", "title")
 
-        groups = []
-        for btn in buttons:
-            if isinstance(btn, TabGroup):
-                groups.append(btn)
-        group_mappings = {group: [] for group in groups}  # tabs within a group
-        single_tabs = [btn for btn in buttons if isinstance(btn, Tab)]  # button without sub tabs
+        tabs = [tab.__dict__ for tab in tabs]
 
-        # associate each tab to its group, if it has one
-        for i in range(len(single_tabs))[::-1]:
-            tab = single_tabs[i]
-            if tab.group is not None:
-                group_mappings[tab.group].append(tab)
-                del single_tabs[i]
-
-        # merge and convert both single tabs and groups keeping their order
-        final_menu = []
-        groups_idx = single_tabs_idx = 0
-        while groups_idx < len(groups) and single_tabs_idx < len(single_tabs):
-            if groups[groups_idx].position == single_tabs[single_tabs_idx].position:
-                if groups[groups_idx].title <= single_tabs[single_tabs_idx].title:
-                    final_menu.append(groups[groups_idx])  # TODO convert to dict
-                    groups_idx += 1
-                else:
-                    final_menu.append(single_tabs[single_tabs_idx])  # TODO convert to dict
-                    single_tabs_idx += 1
-            elif groups[groups_idx].position < single_tabs[single_tabs_idx].position:
-                final_menu.append(groups[groups_idx])  # TODO convert to dict
-                groups_idx += 1
-            else:
-                final_menu.append(single_tabs[single_tabs_idx])  # TODO convert to dict
-                single_tabs_idx += 1
-
-        if groups_idx < len(groups) and len(groups) > 0:
-            for i in range(groups_idx, len(groups)):
-                final_menu.append(groups[i])  # TODO convert to dict
-        elif len(single_tabs) > 0:  # single_tabs_idx < len(single_tabs)
-            final_menu.append(single_tabs[single_tabs_idx:])  # TODO convert to dict
+        for tab in tabs:
+            for key in ["_state", "visible", "position"]:
+                del tab[key]
 
         context = {
-            "tabs": single_tabs,  # TODO final_menu
+            "tabs": tabs,
         }
 
         return render(request, self.template_name, context)
