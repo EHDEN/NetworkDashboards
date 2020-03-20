5aa1402a9a6309112eab9241a53af3cd857e2184
aspedrosa <aspedrosa@ua.pt>
2020-03-20 18:17:30 +0000

    Send sass watcher to background

--- dashboard_viewer/docker-entrypoint.sh
+++ dashboard_viewer/docker-entrypoint.sh
@@ -4,5 +4,5 @@ set -ex
 
 # TODO create two cases: production and development
 
-python manage.py sass -t compressed tabsManager/static/scss/ tabsManager/static/css --watch
+python manage.py sass -t compressed tabsManager/static/scss/ tabsManager/static/css --watch &
 python manage.py runserver 0.0.0.0:8000
