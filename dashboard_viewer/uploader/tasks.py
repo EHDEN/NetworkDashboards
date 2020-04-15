0f0d2cd51a177bac9d25bd31eac9321775bda62e
aspedrosa <aspedrosa@ua.pt>
2020-04-15 14:19:16 +0100

    Remove hardcoded vlues of table and columns names

--- dashboard_viewer/uploader/tasks.py
+++ dashboard_viewer/uploader/tasks.py
@@ -7,7 +7,7 @@ from celery import shared_task
 from django.core.serializers import deserialize
 from django.db import connections
 
-from uploader.models import AchillesResults
+from .models import AchillesResults, AchillesResultsArchive
 
 
 @shared_task
@@ -20,10 +20,28 @@ def update_achilles_results_data(serialized_args, entries):
     if last_upload:
         with closing(connections["achilles"].cursor()) as cursor:
             cursor.execute(
-                """
-                INSERT INTO achilles_results_archive (analysis_id, stratum_1, stratum_2, stratum_3, stratum_4, stratum_5, count_value, data_source_id, upload_info_id)
-                SELECT analysis_id, stratum_1, stratum_2, stratum_3, stratum_4, stratum_5, count_value, %s, %s
-                FROM achilles_results
+                f"""
+                INSERT INTO {AchillesResultsArchive._meta.db_table} (
+                    {AchillesResultsArchive.analysis_id.field_name},
+                    {AchillesResultsArchive.stratum_1.field_name},
+                    {AchillesResultsArchive.stratum_2.field_name},
+                    {AchillesResultsArchive.stratum_3.field_name},
+                    {AchillesResultsArchive.stratum_4.field_name},
+                    {AchillesResultsArchive.stratum_5.field_name},
+                    {AchillesResultsArchive.count_value.field_name},
+                    {AchillesResultsArchive.data_source.field.column},
+                    {AchillesResultsArchive.upload_info.field.column}
+                )
+                SELECT
+                    {AchillesResults.analysis_id.field_name},
+                    {AchillesResults.stratum_1.field_name},
+                    {AchillesResults.stratum_2.field_name},
+                    {AchillesResults.stratum_3.field_name},
+                    {AchillesResults.stratum_4.field_name},
+                    {AchillesResults.stratum_5.field_name},
+                    {AchillesResults.count_value.field_name},
+                    %s, %s
+                FROM {AchillesResults._meta.db_table}
                 """,
                 (db.id, last_upload.id)
             )
