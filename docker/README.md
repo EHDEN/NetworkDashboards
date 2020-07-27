95f9f8b0316507bae345e7addbc03385d586db8c
aspedrosa <aspedrosa@ua.pt>
2020-07-27 14:10:44 +0100

    Adapt repo structure to superset 0.37

--- docker/README.md
+++ docker/README.md
@@ -1,26 +1,19 @@
 # Setup a docker environment
 
-1. Create a `.env` file here, using `.env-example` as reference,
-setting all necessary environment variables
+1. Create a `.env` file on this directory, using `.env-example` as reference, setting all necessary environment variables (SUPERSET\_MAPBOX\_API\_KEY and DASHBOARD\_VIEWER\_SECRET\_KEY)
 
 2. Setup superset
 
-    2.1. Clone the repository
-    
-    `git clone https://github.com/apache/incubator-superset ../superset`
+   2.1 Clone the repository to the root of the repository into a directory called ''superset': `git clone https://github.com/apache/incubator-superset ../superset`
 
-    2.2 Checkout to tag 0.35.1
+   2.2 Checkout to tag 0.37.0rc3: `cd ../superset && git checkout tags/0.37.0rc3 -b tag0.37.0rc3`
 
-    `cd ../superset && git checkout tags/0.35.1 -b tag0.35.1`
+   2.3 Create a `docker-init.sh` file on `docker/superset` based on the file `docker/superset/docker-init-example.sh` to set up admin credentials for superset
 
-3. Set up the database for superset
+3. Set up the database for the dashboard viewer app: `docker-compose run --rm dashboard ./docker-init.sh`
 
-    `docker-compose run --rm superset ./docker-init.sh`
+4. Bring up the containers: `docker-compose up`
 
-4. Set up the database for the dashboard viewer app
-
-    `docker-compose run --rm dashboard ./docker-init.sh`
-
-4. Bring up the containers
-
-    `docker-compose up -d`
+5. If you used the default ports:
+   - Go to `http://localhost` to access the dashboard viewer app.
+   - Go to `http://localhost:8088` to access superset.
--- docker/superset/docker-init-example.sh
+++ docker/superset/docker-init-example.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+# Based on https://github.com/apache/incubator-superset/blob/0.37.0rc3/docker/docker-init.sh
+
 set -e
 
-STEP_CNT=4
+STEP_CNT=3
 
 echo_step() {
 cat <<EOF
@@ -33,9 +20,9 @@ Init Step ${1}/${STEP_CNT} [${2}] -- ${3}
 EOF
 }
 
-# Create an admin user
+# Create an admin user - >> CHANGE THE VALUES BELOW <<
 echo_step "1" "Starting" "Setting up admin user ( admin / admin )"
-superset fab create-admin \ # CHANGE THE VALUES BELOW
+superset fab create-admin \
               --username admin \
               --firstname Superset \
               --lastname Admin \
@@ -48,14 +35,7 @@ echo_step "2" "Starting" "Applying DB migrations"
 superset db upgrade
 echo_step "2" "Complete" "Applying DB migrations"
 
-if [ "$SUPERSET_LOAD_EXAMPLES" = "yes" ]; then
-    # Load some data to play with
-    echo_step "3" "Starting" "Loading examples"
-    superset load_examples
-    echo_step "3" "Complete" "Loading examples"
-fi
-
 # Create default roles and permissions
-echo_step "4" "Starting" "Setting up roles and perms"
+echo_step "3" "Starting" "Setting up roles and perms"
 superset init
-echo_step "4" "Complete" "Setting up roles and perms"
+echo_step "3" "Complete" "Setting up roles and perms"
