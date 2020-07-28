274a0aaf6df6d7dc450d3622463276e97b7b7222
aspedrosa <aspedrosa@ua.pt>
2020-07-28 16:14:17 +0100

    Add cache to superset's configs

--- docker/superset/superset_config_overrides.py
+++ docker/superset/superset_config_overrides.py
@@ -1,27 +1,30 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-#
-# This is an example "local" configuration file. In order to set/override config
-# options that ONLY apply to your local environment, simply copy/rename this file
-# to docker/pythonpath/superset_config_docker.py
-# It ends up being imported by docker/superset_config.py which is loaded by
-# superset/config.py
-#
+
+def get_env_variable(var_name, default=None):
+    """Get the environment variable or raise exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        if default is not None:
+            return default
+        else:
+            error_msg = "The environment variable {} was missing, abort...".format(
+                var_name
+            )
+            raise EnvironmentError(error_msg)
+
 
 PUBLIC_ROLE_LIKE_GAMMA = True
+
+
 ENABLE_ROW_LEVEL_SECURITY = True
+
+
+REDIS_RESULTS_DB = get_env_variable("REDIS_CELERY_DB", 1)
+
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 24 * 60 * 60, # 1 day (in secs)
+    'CACHE_KEY_PREFIX': 'superset_cache',
+    'CACHE_REDIS_URL': f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}',
+}
