b38eed59723f53589f155740f58cdb63bd15cc66
aspedrosa <aspedrosa@ua.pt>
2020-09-25 10:49:26 +0100

    fix black check

--- dashboard_viewer/tabsManager/migrations/0004_auto_20200924_1729.py
+++ dashboard_viewer/tabsManager/migrations/0004_auto_20200924_1729.py
@@ -6,23 +6,23 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tabsManager', '0003_logo'),
+        ("tabsManager", "0003_logo"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='logo',
-            old_name='imageContainerCss',
-            new_name='image_container_css',
+            model_name="logo",
+            old_name="imageContainerCss",
+            new_name="image_container_css",
         ),
         migrations.RenameField(
-            model_name='logo',
-            old_name='imageCss',
-            new_name='image_css',
+            model_name="logo",
+            old_name="imageCss",
+            new_name="image_css",
         ),
         migrations.RenameField(
-            model_name='logo',
-            old_name='imageOnHoverCss',
-            new_name='image_on_hover_css',
+            model_name="logo",
+            old_name="imageOnHoverCss",
+            new_name="image_on_hover_css",
         ),
     ]
