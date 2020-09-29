91924429edfdecfc42f6a11e90b37a063f4d6dc0
Andre Pedrosa <aspedrosa@rust-rules.Portable>
2020-09-29 17:14:20 +0100

    fix

--- dashboard_viewer/tabsManager/templatetags/custom_tags.py
+++ dashboard_viewer/tabsManager/templatetags/custom_tags.py
@@ -1,8 +1,9 @@
 from django import template
+from pydoc import locate
 
 register = template.Library()
 
 
 @register.filter
 def isinst(val, class_str):
-    return isinstance(val, class_str)
+    return isinstance(val, locate(class_str))
