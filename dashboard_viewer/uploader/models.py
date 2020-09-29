47f05faa3688853ec10411098abb4b5c5cbca180
aspedrosa <aspedrosa@ua.pt>
2020-09-29 18:07:51 +0100

    Create concept model

--- dashboard_viewer/uploader/models.py
+++ dashboard_viewer/uploader/models.py
@@ -127,3 +127,23 @@ class AchillesResultsArchive(models.Model):
     stratum_4 = models.TextField(null=True)
     stratum_5 = models.TextField(null=True)
     count_value = models.BigIntegerField()
+
+
+class Concept(models.Model):
+    class Meta:
+        db_table = "concept"
+        #indexes = [
+        #    models.Index(fields=("concept_id",)),
+        #    models.Index(fields=("concept_name",)),
+        #]
+
+    concept_id = models.BigIntegerField(primary_key=True)
+    concept_name = models.CharField(max_length=255)
+    domain_id = models.CharField(max_length=20)
+    vocabulary_id = models.CharField(max_length=20)
+    concept_class_id = models.CharField(max_length=20)
+    standard_concept = models.CharField(max_length=1, null=True)
+    concept_code = models.CharField(max_length=50)
+    valid_start_date = models.DateField()
+    valid_end_date = models.DateField()
+    invalid_reason = models.CharField(max_length=1, null=True)
