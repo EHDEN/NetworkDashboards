5aa2f2a838b05bb5a11eb5c5fae7b54e96cfb430
aspedrosa <aspedrosa@ua.pt>
2020-10-01 11:47:02 +0100

    Fix black checks

--- dashboard_viewer/uploader/migrations/0004_auto_20201001_1037.py
+++ dashboard_viewer/uploader/migrations/0004_auto_20201001_1037.py
@@ -6,13 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('uploader', '0003_merge_20200923_1703'),
+        ("uploader", "0003_merge_20200923_1703"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='datasource',
-            name='release_date',
-            field=models.DateField(help_text='Date at which DB is available for research for current release.', null=True),
+            model_name="datasource",
+            name="release_date",
+            field=models.DateField(
+                help_text="Date at which DB is available for research for current release.",
+                null=True,
+            ),
         ),
     ]
