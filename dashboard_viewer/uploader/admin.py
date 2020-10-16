3ca454c2489ca284deca1a4f80ff09885e66e754
aspedrosa <aspedrosa@ua.pt>
2020-10-16 11:48:43 +0100

    apply changes

--- dashboard_viewer/uploader/admin.py
+++ dashboard_viewer/uploader/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Country, DatabaseType, DataSource
+from .models import Country, DatabaseType, DataSource, UploadHistory
 
 admin.site.register(DataSource)
 admin.site.register(Country)
 admin.site.register(DatabaseType)
+admin.site.register(UploadHistory)
