8599cb2b356353a66c89d387e3bebc0b2cbbf648
aspedrosa <aspedrosa@ua.pt>
2020-10-16 12:03:16 +0100

    Use admin models

--- dashboard_viewer/tabsManager/admin.py
+++ dashboard_viewer/tabsManager/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Tab, TabGroup
 
-admin.site.register(Tab)
-admin.site.register(TabGroup)
+
+@admin.register(Tab)
+class TabAdmin(admin.ModelAdmin):
+    list_display = ("title", "position", "visible", "group")
+    list_filter = ("visible",)
+
+
+@admin.register(TabGroup)
+class TabAdmin(admin.ModelAdmin):
+    list_display = ("title", "position", "visible")
+    list_filter = ("visible",)
--- dashboard_viewer/uploader/admin.py
+++ dashboard_viewer/uploader/admin.py
@@ -5,4 +5,11 @@ from .models import Country, DatabaseType, DataSource, UploadHistory
 admin.site.register(DataSource)
 admin.site.register(Country)
 admin.site.register(DatabaseType)
-admin.site.register(UploadHistory)
+
+
+@admin.register(UploadHistory)
+class UploadHistoryAdmin(admin.ModelAdmin):
+    list_display = ("data_source", "upload_date")
+
+    def has_add_permission(self, *_, **__):
+        return False
