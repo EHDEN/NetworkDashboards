5b293925f93907073fe9efb7ac36cf4159f93727
aspedrosa <aspedrosa@ua.pt>
2020-10-20 10:50:28 +0100

    fix github actions checks

--- dashboard_viewer/materialized_queries_manager/admin.py
+++ dashboard_viewer/materialized_queries_manager/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import MaterializedQuery
 
+
 @admin.register(MaterializedQuery)
 class MaterializedQueryAdmin(admin.ModelAdmin):
-    list_display = ("name", "dashboards",)
+    list_display = (
+        "name",
+        "dashboards",
+    )
