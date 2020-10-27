628729a93af8ae04f0da64dcd5a05f3d509e7d1e
aspedrosa <aspedrosa@ua.pt>
2020-10-27 20:14:23 +0000

    Create Superset url variable to fill tabs url

--- dashboard_viewer/docker-init.sh
+++ dashboard_viewer/docker-init.sh
@@ -10,6 +10,12 @@ python manage.py migrate
 python manage.py migrate --database=achilles
 
 # Load initial data
+if [ $SUPERSET_URL == "" ] ; then
+    1>&2 echo "Environment variable SUPERSET_URL not set"
+    exit 1
+fi
+
+sed -i s/"{SUPERSET_URL}"/$SUPERSET_URL/ /app/dashboard_viewer/tabsManager/fixtures/initial_data.json
 python manage.py loaddata --app tabsManager initial_data
 python manage.py loaddata --app uploader --database achilles country_initial_data
 python manage.py loaddata --app materialized_queries_manager --database achilles initial_data
