9caae546b79f4181c4a1bf7de02f84795a9e45fd
aspedrosa <aspedrosa@ua.pt>
2020-12-12 16:37:42 +0000

    Implement changes

--- backups/upload_to_server.py
+++ backups/upload_to_server.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
 
+import datetime
+import os
 import sys
 
 import mega
 
-UPLOAD_FOLDER = "dashboards_backups"
+UPLOAD_DIRECTORY = "dashboards_backups"
+CURRENT_WEEK_DIRECTORY = "current_week"
+
+
+def get_or_create_directory(m: mega.Mega, name: str, path: str):
+    """
+    :param m: Mega object
+    :param name: name of the directory to upload. Used for error messages
+    :param path: path to the directory to get or create
+    """
+    directory = m.find(path)
+    if not directory:
+        return m.create_folder(path)[os.path.basename(path)]
+
+    if directory[1]["t"] != 1:  # if its not a directory
+        print(
+            f"Node on the {name} directory provided is not a directory.",
+            file=sys.stderr,
+        )
+        sys.exit(4)
+
+    return directory[0]
 
 
 def main(argc, argv):
@@ -30,20 +53,44 @@ def main(argc, argv):
         print(f"Error while logging in to Mega. ({err})", file=sys.stderr)
         sys.exit(3)
 
-    if UPLOAD_FOLDER:
-        upload_folder = m.find(UPLOAD_FOLDER)
-        if not upload_folder:
-            upload_folder = m.create_folder(UPLOAD_FOLDER)[UPLOAD_FOLDER]
-        else:
-            upload_folder = upload_folder[0]
+    if UPLOAD_DIRECTORY:
+        upload_folder = get_or_create_directory(m, "upload directory", UPLOAD_DIRECTORY)
     else:
         upload_folder = None
 
-    try:
-        m.upload(argv[2], upload_folder)
-    except FileNotFoundError:
-        print(f'Upload file "{argv[2]}" not found', file=sys.stderr)
-        sys.exit(3)
+    current_week = get_or_create_directory(
+        m,
+        "current week",
+        CURRENT_WEEK_DIRECTORY
+        if upload_folder is None
+        else os.path.join(UPLOAD_DIRECTORY, CURRENT_WEEK_DIRECTORY),
+    )
+
+    current_week_files = m.get_files_in_node(current_week)
+    if len(current_week_files) == 6:
+        # if we already got 6 files on the current week directory
+        #  then the current file is the the final one for this week
+
+        _, extension = os.path.splitext(argv[2])
+        try:
+            m.upload(
+                argv[2],
+                upload_folder,
+                datetime.datetime.now().strftime("%Y%W") + extension,  # YearWeek.tar.gz
+            )
+        except FileNotFoundError:
+            print(f'Upload file "{argv[2]}" not found', file=sys.stderr)
+            sys.exit(3)
+
+        for file in current_week_files:
+            m.delete(file)
+        m.empty_trash()
+    else:
+        try:
+            m.upload(argv[2], current_week)
+        except FileNotFoundError:
+            print(f'Upload file "{argv[2]}" not found', file=sys.stderr)
+            sys.exit(3)
 
 
 if __name__ == "__main__":
