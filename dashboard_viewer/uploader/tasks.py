01cd55c1f6dafe65f4a45c6aa3a154aee1c2299f
aspedrosa <aspedrosa@ua.pt>
2021-01-07 10:47:58 +0000

    skip and log unexpected errors

--- dashboard_viewer/uploader/tasks.py
+++ dashboard_viewer/uploader/tasks.py
@@ -9,6 +9,7 @@ from celery.utils.log import get_task_logger
 from django.conf import settings
 from django.core.cache import cache
 from django.db import connections, ProgrammingError
+
 from materialized_queries_manager.models import MaterializedQuery
 from redis_rw_lock import RWLock
 
@@ -132,9 +133,17 @@ def update_achilles_results_data(
                         f"REFRESH MATERIALIZED VIEW {materialized_query.name}"
                     )
                 except ProgrammingError:
+                    # If this happen, it is assumed its because the there is no materialized views
+                    #  created on postgres with the given name
                     # TODO Log this or give some feed back on the Materialized query list, on admin app, if any
                     #  record doesn't have a materialized view associated.
                     pass  # Ignore if the view doesn't exist
+                except:  # noqa
+                    logger.exception(
+                        "Some unexpected error happen while refreshing materialized query %s. [datasource %d]",
+                        materialized_query.name,
+                        db_id,
+                    )
 
         write_lock.release()
 
