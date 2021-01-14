f0ee15fb9b81fe3ee743773eeecb6ced71c11b2b
aspedrosa <aspedrosa@ua.pt>
2021-01-14 15:24:31 +0000

    remove unused superset configs

--- docker/superset/superset_config_overrides.py
+++ docker/superset/superset_config_overrides.py
@@ -15,23 +15,10 @@ def get_env_variable(var_name, default=None):
         raise EnvironmentError(error_msg)
 
 
-PUBLIC_ROLE_LIKE_GAMMA = True
-
-
-ENABLE_ROW_LEVEL_SECURITY = True
-
-
 REDIS_HOST = get_env_variable("REDIS_HOST")
 REDIS_PORT = get_env_variable("REDIS_PORT")
 REDIS_CACHE_DB = get_env_variable("REDIS_CACHE_DB", 3)
 REDIS_CELERY_DB = get_env_variable("REDIS_CELERY_DB", 4)
-
-CACHE_CONFIG = {
-    "CACHE_TYPE": "redis",
-    "CACHE_DEFAULT_TIMEOUT": 24 * 60 * 60,  # 1 day (in secs)
-    "CACHE_KEY_PREFIX": "superset_cache",
-    "CACHE_REDIS_URL": f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CACHE_DB}",
-}
 
 
 class CeleryConfig:
