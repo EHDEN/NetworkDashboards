07933011a6c33acaf7e68608625790f4d6554386
aspedrosa <aspedrosa@ua.pt>
2021-03-17 16:26:32 +0000

    add tests to single mode application features

--- dashboard_viewer/uploader/tests.py
+++ dashboard_viewer/uploader/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings, tag
 
-# Create your tests here.
+
+@tag("third-party-app")
+@override_settings(ALLOWED_HOSTS=["thisapp.host.com", "mainapp.host.com"])
+class UploaderRestrictedAccess(TestCase):
+    databases = "__all__"
+
+    def test_block_if_wrong_host(self):
+        response = self.client.get("/uploader/test/", HTTP_HOST="thisapp.host.com")
+
+        self.assertEqual(403, response.status_code)
+
+    def test_not_block_if_correct_host(self):
+        response = self.client.get("/uploader/test/", HTTP_HOST="mainapp.host.com")
+
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(response.has_header("X-Frame-Options"))
+        self.assertEqual("ALLOW-FROM HTTPS://MAINAPP.HOST.COM/", response["X-Frame-Options"])
+
+    def test_not_block_other_urls(self):
+        response = self.client.get("/admin/login/", HTTP_HOST="thisapp.host.com")
+
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(response.has_header("X-Frame-Options"))
+        self.assertEqual("ALLOW-FROM HTTPS://MAINAPP.HOST.COM/", response["X-Frame-Options"])
+
+
+class UploaderNonRestrictedAccess(TestCase):
+    databases = "__all__"
+
+    @override_settings(ALLOWED_HOSTS=["some.domain.com"])
+    def test_not_block_if_single_application(self):
+        response = self.client.get("/uploader/test/", HTTP_HOST="some.domain.com")
+
+        self.assertEqual(200, response.status_code)
+        if response.has_header("X-Frame-Options"):
+            self.assertNotIn("ALLOW-FROM ", response.get("X-Frame-Options"))
