6a1ba00d8316945603b0d1f0e4084a9d3d245b0f
aspedrosa <aspedrosa@ua.pt>
2021-04-09 17:30:26 +0100

    only need for one migrate command

--- dashboard_viewer/docker-entrypoint.sh
+++ dashboard_viewer/docker-entrypoint.sh
@@ -3,12 +3,12 @@
 set -ex
 
 python manage.py migrate
-python manage.py migrate --database=achilles uploader
-python manage.py migrate --database=achilles materialized_queries_manager
+python manage.py migrate --database=achilles
+
+python manage.py compilescss
+python manage.py collectstatic --noinput --ignore="*.scss"
 
 if [ "${DASHBOARD_VIEWER_ENV}" = "production" ]; then
-    python manage.py compilescss
-    python manage.py collectstatic --noinput --ignore="*.scss"
     exec gunicorn dashboard_viewer.wsgi:application --bind 0.0.0.0:8000 --workers 5
 else
     python manage.py runserver 0.0.0.0:8000
