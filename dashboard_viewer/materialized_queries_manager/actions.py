646245d78f55275ebdb3454302d93168d11fb6f8
aspedrosa <aspedrosa@ua.pt>
2022-01-04 19:52:07 +0000

    actually fix the timeout problem of materialized views

--- dashboard_viewer/materialized_queries_manager/actions.py
+++ dashboard_viewer/materialized_queries_manager/actions.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.utils.translation import gettext as _, gettext_lazy
 
 from .tasks import refresh_materialized_views_task
@@ -12,6 +13,8 @@ def refresh_materialized_views_action(model_admin, request, queryset):
         _("Refreshing materialized view(s) on a background task."),
         messages.SUCCESS,
     )
+
+    return HttpResponseRedirect("/admin/")
 
 
 refresh_materialized_views_action.short_description = gettext_lazy(
